ebe5ba56f3cea4e596b126ee99d9bd0a6241d159
stela <stella.plamenova@gmail.com>
2018-01-24 10:20:46 +0100

    project set up

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+
+
+// task: delete build/*
+var del = require('del');
+
+gulp.task('clean', function(callback) {
+	del(['ship/**'], callback);
+});
+
+// process html
+gulp.task('html', function() {
+	gulp.src('index.html')
+		.pipe(gulp.dest('ship'))
+		.pipe(connect.reload());
+});
+
+// process css
+var less = require('gulp-less');
+var LessPluginCleanCss = require('less-plugin-clean-css');
+var cleanCss = new LessPluginCleanCss({advanced: true});
+
+
+gulp.task('less', function() {
+	gulp.src('styles/*.less')
+		.pipe(less({
+			plugins: [cleanCss]
+		}))
+		.pipe(gulp.dest('ship/styles'))
+		.pipe(connect.reload());
+});
+
+// task: watch
+gulp.task('watch', function() {
+	gulp.watch('index.html', ['html']);
+	gulp.watch('styles/**/*', ['less']);
+	gulp.watch('scripts/**/*', ['scripts']);
+});
+
+// connect server
+var connect = require('gulp-connect');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'ship',
+		port: 9000,
+		livereload: true
+	});
+});
+
+// task: default
+gulp.task('default', ['clean'], function() {
+	gulp.start([
+		'html',
+		'less',
+		'connect',
+		'watch'
+	]);
+});
